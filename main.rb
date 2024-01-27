0662e07633a7b11d461905cab2deda1c76f3a8f2
JaraSanchezLuis <107270563+luisenjs@users.noreply.github.com>
2024-01-26 20:08:41 -0500

    Scraping terminado

--- main.rb
+++ main.rb
@@ -21,6 +21,16 @@ class URLResolver
     end
   end
 
+  def alreadyExist(archivo, data)
+    CSV.foreach(archivo, headers: true) do |row|
+      if row[0] == data[0]
+        return true
+      else
+        return false
+      end
+    end
+  end
+
   def header(archivo, top_line)
     CSV.open(archivo, 'w') do |csv|
       csv << top_line
@@ -65,7 +75,7 @@ end
 
 juegos_dicc = {}
 
-doc = URLResolver.new("https://store.steampowered.com/search/?supportedlang=spanish&tags=21&filter=topsellers&ndl=1").resolver()
+doc = URLResolver.new("https://store.steampowered.com/search/?supportedlang=latam&tags=1742%2C492%2C1664&filter=topsellers&ndl=1").resolver()
 games = doc.css('div#search_resultsRows')
 games.css("a").each do |game|
   juego = game.css("span.title").text.gsub("®", "")
@@ -102,7 +112,9 @@ if juegos_dicc.key?(juego)
       datosInfo << "No info"
     end
     datosInfo << detalles.at("b:contains('Date:')").next.text.strip
-    rawData.saveData("InfoJuegos.csv", datosInfo, "a")
+    unless rawData.alreadyExist("InfoJuegos.csv", datosInfo)
+      rawData.saveData("InfoJuegos.csv", datosInfo, "a")
+    end
     logros = GameAchivements.new(juegos_dicc[juego]["logros"], juego)
     logros.calcularLogros()
   else
@@ -110,4 +122,47 @@ if juegos_dicc.key?(juego)
   end
 else
   puts "No se encontro el juego #{juego}"
+end
+
+puts "Vamos a comparar juegos por su precio y disponibilidad"
+
+juegos_a_comparar = []
+max_intentos = 3
+intentos = 0
+
+while intentos < max_intentos
+  puts "Ingrese el nombre del juego #{intentos + 1}: "
+  juego = gets.chomp.downcase
+
+  if juegos_dicc.key?(juego)
+    juegos_a_comparar << juego
+    intentos += 1
+  else
+    puts "#{juego} no existe. Intenta nuevamente."
+  end
+end
+
+comp = URLResolver.new("")
+comp.clear("comparacion.csv")
+comp.header("comparacion.csv", ["Nombre", "Plataformas", "Precio"])
+
+juegos_a_comparar.each do |juego|
+  rawData = URLResolver.new(juegos_dicc[juego]["link"])
+  docjuego = rawData.resolver()
+  gamePurcharseArea = docjuego.css(".game_area_purchase_game_wrapper")
+  gamePurcharseArea.each do |info|
+    infojuegos = []
+    nombre = info.css("h1").text.strip
+    plataformas = info.css('.game_area_purchase_platform span').map { |span| span['class'].split.last }
+    infojuegos << nombre
+    infojuegos << plataformas.join("|")
+    if info.css(".game_purchase_price.price").text.empty?
+      precio = info.css(".discount_final_price").text.strip
+      infojuegos << precio
+    else
+      precio = info.css(".game_purchase_price.price").text.strip
+      infojuegos << precio
+    end
+    rawData.saveData("comparacion.csv", infojuegos, "a")
+  end
 end
